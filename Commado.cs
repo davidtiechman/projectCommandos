45eb39b4f46b92efb6461147b4ea9e2d31acf542
David Tiechman <d0534110171@gmail.com>
2025-05-27 10:23:50 +0300

    creat one class

---
+++ Commado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comando
+{
+    public class Commado
+    {
+        public string Name;
+        public string CodeName;
+        public string[] ArryaTools;
+        public string Status;
+        public Commado(string name, string codename, string status)
+        {
+            this.Name = name;
+            this.CodeName = codename;
+            this.Status = status;
+            this.ArryaTools = new string[] {"Hammer", "Chisel", "Rope", "Bag", "Bottle"};
+            
+        }
+    }
+}
